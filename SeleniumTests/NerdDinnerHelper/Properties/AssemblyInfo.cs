c22882cf43b5d8d0a77dfe0b6f6f4b9046135475
Sunny Gulati <sunnywiz@gmail.com>
2012-08-24 14:02:36 -0400

    Initial version with two tests
    No Page helpers or any of that stuff yet

---
+++ SeleniumTests/NerdDinnerHelper/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NerdDinnerHelper.cs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NerdDinnerHelper.cs")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0826e467-2713-471a-b6d2-96f826333bb4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SeleniumTests/SeleniumTests/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumTests
+{
+    [TestFixture]
+    public class NerdDinnerTests
+    {
+        [Test]
+        public void MainPage_CanSearchForDinners_ButNeverFindsAnyInKY()
+        {
+            using (IWebDriver driver = new FirefoxDriver())
+            {
+                driver.Navigate().GoToUrl("http://www.nerddinner.com");
+                var input = driver.FindElement(By.Id("Location"));
+                input.SendKeys("40056");
+                var search = driver.FindElement(By.Id("search"));
+                search.Click();
+                var results = driver.FindElements(By.ClassName("dinnerItem"));
+                // at this point, i don't know what to do, as there's never any search results.   
+                Assert.AreEqual(0, results.Count,
+                                "No dinners should be found.. omg, if this works, then its worth it to change the test");
+            }
+        }
+
+        [Test]
+        public void MainPage_CanSeeAllDinners_ShouldFindAFew()
+        {
+            using (IWebDriver driver = new FirefoxDriver())
+            {
+                driver.Navigate().GoToUrl("http://www.nerddinner.com");
+
+                var searchTextDiv = (from e in driver.FindElements(By.ClassName("search-text"))
+                                     where e.TagName == "div"
+                                     select e).FirstOrDefault();
+                Assert.IsNotNull(searchTextDiv, "Should find search text which has the view all link");
+                var viewAllDinnerslink = searchTextDiv.FindElement(By.TagName("a"));
+                Assert.IsNotNull(viewAllDinnerslink, "should find the view all upcoming dinners link");
+                viewAllDinnerslink.Click();
+
+                // now on /Dinners page
+                var upcomingDinnersUl = (from e in driver.FindElements(By.ClassName("upcomingdinners"))
+                                          where e.TagName == "ul"
+                                          select e).FirstOrDefault();
+                Assert.IsNotNull(upcomingDinnersUl,"should find a Upcoming Dinners UL");
+                var dinnerLinks = upcomingDinnersUl.FindElements(By.TagName("a")); 
+                Assert.Greater(dinnerLinks.Count, 0, "There should be at least one dinner out there.. come on guys");
+            }
+        }
+    }
+}
